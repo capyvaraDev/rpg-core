46ee287e09d54943b912b0b3acc3de9cc399f093
Bonekazz <juracirdantas@gmail.com>
2024-05-25 04:40:53 -0300

    'else' eca

--- core/inventory.py
+++ core/inventory.py
@@ -17,15 +17,14 @@ class Inventory(BaseModel):
         ]
 
     def add(self, item: Item):
-        if len(self.items) < self.max_size:
-            self.items.append(item)
-            print(f"Item '{item.name}' adicionado ao inventário")
-        else:
+        if len(self.items) == self.max_size:
             print(f"O inventário está cheio. Não foi possível adicionar {item.name}")
+            return
+        self.items.append(item)
+        print(f"Item '{item.name}' adicionado ao inventário")
 
     def rem(self, item: Item):
-        if item in self.items:
-            self.items.remove(item)
-            print(f"Item '{item.name}' removido ao inventário")
-        else:
+        if item not in self.items:
             print("O item especificado não está no inventário")
+        self.items.remove(item)
+        print(f"Item '{item.name}' removido ao inventário")
